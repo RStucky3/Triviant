6ecc19c1564e623ee3b673d10b5e23401d2a2116
RStucky3 <302007406@student.rocmondriaan.nl>
2020-09-25 14:14:17 +0200

    triviant

--- Triviant/Triviant/Program.cs
+++ Triviant/Triviant/Program.cs
@@ -6,7 +6,8 @@ namespace Triviant
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Triviant!");
+
         }
     }
 }
