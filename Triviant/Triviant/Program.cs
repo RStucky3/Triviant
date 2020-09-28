8871a4a759a08e9cf8cc2c8d8150c1a2e7723c8c
RStucky3 <302007406@student.rocmondriaan.nl>
2020-09-28 10:03:36 +0200

    triviantversie1

--- Triviant/Triviant/Program.cs
+++ Triviant/Triviant/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Triviant
 {
     class Program
     {
         static int Score = 0;
-        static string[] Vraag = { "Wat is de naam van het tweede deel uit The Matrix trilogie?", "Welke actrice speelde Monica Gellar in de serie Friends?", "", "" };
-        static string[,] Antwoorden = { { "The Matrix Reloaded", "The Matrix Revolutions", "The Matrix", "The Real Matrix" }{ "Jennifer Aniston", "Courteney Cox", "Lisa Kudrow", "David Schwimmer" }} ;
-        static int[] JuisteAntwoord = { 0, 1, 0, 0 };
-
+        static string[] Vraag = { "Wat is de naam van het tweede deel uit The Matrix trilogie?", "Welke actrice speelde Monica Gellar in de serie Friends?", "Voor welke inlichtingendienst was James Bond een werkzaam?", "Welk bedrijf maakte als eerst een volledige 3d-animatiefilm?" };
+        static string[,] Antwoorden = { {"The Matrix Reloaded", "The Matrix Revolutions", "The Matrix", "The Real Matrix"}, { "Jennifer Aniston", "Courteney Cox", "Lisa Kudrow", "David Schwimmer" }, { "CIA", "AIVD", "FBI", "MI6" }, { "Disney", "Pixar", "Apple", "Dreamworks" } } ;
+        static int[] JuisteAntwoord = { 0, 1, 3, 1 };
+        static int index = 0;       
 
         static void Main(string[] args)
         {
@@ -17,12 +19,15 @@ namespace Triviant
             Console.WriteLine("U krijgt vragen te zien en daarbij 4 antwoorden, kiest daarbij het beste antwoord.");
             Console.WriteLine("Klik op enter om te beginnen!");
             Console.ReadLine();
-       
-                showVraag(1);
-                showAntwoorden();
-            
 
-                try {
+            do
+            {
+                showVraag(index + 1);
+                showAntwoorden();
+
+
+                try
+                {
                     int Antwoord = Convert.ToInt32(Console.ReadLine());
 
                     Console.WriteLine("");
@@ -33,7 +38,7 @@ namespace Triviant
                             Score++;
                             break;
                         case 0:
-                            Console.WriteLine("Dat is helaas niet goed. Het antwoord is " + Antwoorden[JuisteAntwoord]);
+                            Console.WriteLine("Dat is helaas niet goed. Het antwoord is " + Antwoorden[index, JuisteAntwoord[index]]);
                             break;
                         case 99:
                             Console.WriteLine("Dit is geen gelde invoer.");
@@ -46,10 +51,17 @@ namespace Triviant
                     Console.WriteLine(e.Message);
                 }
 
-
+                index++;
                 Console.WriteLine("Je hebt " + Score + " punt(en).");
+                Console.WriteLine("Klik op enter om door te gaan");
 
+                Console.ReadLine();
+            } while (index < 4);
+            Console.WriteLine("Gefeliciteerd, u heeft de quiz uitgespeelt.");
+            Console.WriteLine("En daarbij een score behaalt van " + Score + " punt(en).");
+            Console.WriteLine("Goed gedaan!");
             Console.ReadLine();
+
         }
 
 
@@ -57,7 +69,7 @@ namespace Triviant
         static void showVraag(int Nr)
         {
             Console.WriteLine("Vraag "+ Nr + ":");
-            Console.WriteLine(Vraag);
+            Console.WriteLine(Vraag[index]);
             Console.WriteLine("");
            
         }
@@ -67,20 +79,24 @@ namespace Triviant
         static void showAntwoorden()
         {
             int teller = 0;
-            foreach (string antwoord in Antwoorden)
+
+            for (int i = 0; i <= 3; i++)
             {
                 teller++;
-                Console.WriteLine((teller) + ". " + antwoord);        
+                string antwoord = Antwoorden[index,i]; 
+                Console.WriteLine(teller + ". " + antwoord);
             }
+
             Console.WriteLine("");
             Console.WriteLine("Type uw keuze: 1,2,3 of 4");
+
         }
 
 
 
         static int checkAntwoord(int Antw)
         {
-            if (Antw == JuisteAntwoord + 1) 
+            if (Antw == JuisteAntwoord[index] + 1) 
             {
                 return 1;
             }
